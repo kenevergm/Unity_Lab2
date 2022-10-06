193dadbcc14b9659a0c11e18cb1a8c73ae5af3f5
Vasiliy <kenevergm@gmail.com>
2022-10-06 21:38:16 +0300

    add sdk

---
+++ Assets/Examples/Scripts/AdvertisementPanel.cs
@@ -0,0 +1,96 @@
+﻿using InstantGamesBridge;
+using InstantGamesBridge.Modules.Advertisement;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Examples
+{
+    public class AdvertisementPanel : MonoBehaviour
+    {
+        [SerializeField] private Text _interstitialState;
+
+        [SerializeField] private Text _rewardedState;
+
+        [SerializeField] private InputField _minimumDelayBetweenInterstitial;
+
+        [SerializeField] private Button _setMinimumDelayBetweenInterstitialButton;
+
+        [SerializeField] private Button _showInterstitialButton;
+
+        [SerializeField] private Toggle _showInterstitialIgnoreDelayToggle;
+
+        [SerializeField] private Button _showRewardedButton;
+
+        [SerializeField] private GameObject _overlay;
+
+
+        private void OnEnable()
+        {
+            Bridge.advertisement.interstitialStateChanged += OnInterstitialStateChanged;
+            Bridge.advertisement.rewardedStateChanged += OnRewardedStateChanged;
+            _setMinimumDelayBetweenInterstitialButton.onClick.AddListener(OnSetMinimumDelayBetweenInterstitialButtonClicked);
+            _showInterstitialButton.onClick.AddListener(OnShowInterstitialButtonClicked);
+            _showRewardedButton.onClick.AddListener(OnShowRewardedButtonClicked);
+
+            OnInterstitialStateChanged(Bridge.advertisement.interstitialState);
+            OnRewardedStateChanged(Bridge.advertisement.rewardedState);
+            UpdateMinimumDelayBetweenInterstitial();
+        }
+
+        private void OnDisable()
+        {
+            Bridge.advertisement.interstitialStateChanged -= OnInterstitialStateChanged;
+            Bridge.advertisement.rewardedStateChanged -= OnRewardedStateChanged;
+            _setMinimumDelayBetweenInterstitialButton.onClick.RemoveAllListeners();
+            _showInterstitialButton.onClick.RemoveAllListeners();
+            _showRewardedButton.onClick.RemoveAllListeners();
+        }
+
+
+        private void OnInterstitialStateChanged(InterstitialState state)
+        {
+            _interstitialState.text = $"Interstitial State: { state }";
+        }
+
+        private void OnRewardedStateChanged(RewardedState state)
+        {
+            _rewardedState.text = $"Rewarded State: { state }";
+        }
+
+        private void OnSetMinimumDelayBetweenInterstitialButtonClicked()
+        {
+            int.TryParse(_minimumDelayBetweenInterstitial.text, out var seconds);
+            Bridge.advertisement.SetMinimumDelayBetweenInterstitial(seconds);
+            UpdateMinimumDelayBetweenInterstitial();
+        }
+
+        private void OnShowInterstitialButtonClicked()
+        {
+            _overlay.SetActive(true);
+
+            var ignoreDelay = _showInterstitialIgnoreDelayToggle.isOn;
+
+            // Common variant
+            Bridge.advertisement.ShowInterstitial(
+                ignoreDelay, 
+                success => { _overlay.SetActive(false); });
+
+            // Platform specific variant
+            /*Bridge.advertisement.ShowInterstitial(
+                success => { _overlay.SetActive(false); },
+                new ShowInterstitialVkOptions(ignoreDelay),
+                new ShowInterstitialYandexOptions(ignoreDelay));*/
+        }
+
+        private void OnShowRewardedButtonClicked()
+        {
+            _overlay.SetActive(true);
+            Bridge.advertisement.ShowRewarded(success => { _overlay.SetActive(false); });
+        }
+
+        private void UpdateMinimumDelayBetweenInterstitial()
+        {
+            _minimumDelayBetweenInterstitial.text = Bridge.advertisement.minimumDelayBetweenInterstitial.ToString();
+        }
+    }
+}
---
+++ Assets/Examples/Scripts/Example.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using InstantGamesBridge;
+using InstantGamesBridge.Modules.Game;
+using UnityEngine;
+
+namespace Examples
+{
+    public class Example : MonoBehaviour
+    {
+        [SerializeField] private GameObject _initializationPanel;
+
+        [SerializeField] private GameObject _initializationErrorPanel;
+
+        [SerializeField] private List<GameObject> _otherPanels;
+
+        [SerializeField] private AudioSource _musicAudioSource;
+
+        private void Start()
+        {
+            _initializationPanel.SetActive(true);
+            _initializationErrorPanel.SetActive(false);
+
+            foreach (var panel in _otherPanels)
+                panel.SetActive(false);
+
+            Bridge.Initialize(OnInitializationCompleted);
+        }
+
+        private void OnInitializationCompleted(bool isInitialized)
+        {
+            if (isInitialized)
+            {
+                _initializationPanel.SetActive(false);
+
+                foreach (var panel in _otherPanels)
+                    panel.SetActive(true);
+
+                _musicAudioSource.Play();
+                Bridge.game.visibilityStateChanged += OnGameVisibilityStateChanged;
+            }
+            else
+                _initializationErrorPanel.SetActive(true);
+        }
+
+        private void OnGameVisibilityStateChanged(VisibilityState visibilityState)
+        {
+            switch (visibilityState)
+            {
+                case VisibilityState.Visible:
+                    _musicAudioSource.Play();
+                    break;
+
+                case VisibilityState.Hidden:
+                    _musicAudioSource.Pause();
+                    break;
+            }
+        }
+    }
+}
